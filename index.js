7abe25eda1100cce1f74fe30872c30832668abfd
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-12-24 15:19:01 +0530

    hosted new frontend for existing backend

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const app = Express();
 
 const PORT = process.env.PORT || 8001;
 
-app.use(cors({ credentials: true, origin:"https://learnplusplus.vercel.app"}));
-//http://localhost:3000
+app.use(cors({ credentials: true, origin:"https://learnplusplus-six.vercel.app"}));
+//http://localhost:3000, https://learnplusplus.vercel.app
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
